e39b480d781802dcb5d74c3148dfd95a02490229
Anderson Lorencini <lorencini.brdev@gmail.com>
2023-05-28 20:43:42 -0300

    Added a basic GameState.cs framework.

---
+++ Engine/GameStates/GameState.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using VoodooFramework.Engine.Input;
+
+namespace VoodooFramework.Engine.GameStates;
+
+public abstract class GameState
+{
+    protected GameState(GameWindow game)
+    {
+        Game = game;
+    }
+
+    public abstract bool Init();
+    public abstract void CleanUp();
+
+    public abstract void Pause();
+    public abstract void Resume();
+    
+    public abstract void HandleEvents(InputManager inputManager);
+    public abstract void Update(GameTime gameTime);
+    public abstract void Draw(SpriteBatch spriteBatch, GameTime delta);
+    
+    //Protected Properties:
+    protected GameWindow Game { get; }
+}
---
+++ Engine/GameStates/GameStateManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using VoodooFramework.Engine.Input;
+
+namespace VoodooFramework.Engine.GameStates;
+
+public class GameStateManager
+{
+    //Private Members:
+    private readonly Stack<GameState> _loadedStates = new();
+
+    public void ChangeState(GameState state)
+    {
+        //Removing all current states:
+        while (_loadedStates.Count > 0) 
+            _loadedStates.Pop().CleanUp();
+
+        //Initializing the new state:
+        if (!state.Init())
+            throw new Exception("FATAL: Failed to initialize the requested GameState. Exiting.");
+        
+        //Saving:
+        _loadedStates.Push(state);
+    }
+
+    public void HandleEvents(InputManager inputManager)
+    {
+        _loadedStates.Peek().HandleEvents(inputManager);
+    }
+    
+    public void Update(GameTime gameTime)
+    {
+        _loadedStates.Peek().Update(gameTime);
+    }
+    
+    public void Draw(SpriteBatch spriteBatch, GameTime delta)
+    {
+        _loadedStates.Peek().Draw(spriteBatch, delta);
+    }
+}
